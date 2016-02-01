d9305449b164fdc3f3c3414e8282a0a63f4efcfd
Marco Pessotto <melmothx@gmail.com>
2016-02-01 17:52:56 +0100

    Added helper script

---
+++ build-and-upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+dir=$1
+
+if [ ! -d "$dir" ]; then
+    echo "$dir is not a directory"
+    exit 2;
+fi
+
+rm -v ${dir}*.{deb,build,changes}
+cd $dir
+debuild -i -us -uc -b
+cd ..
+scp ${dir}*.deb melmothx@packages.amusewiki.org:
